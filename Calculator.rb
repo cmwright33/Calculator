c4cb695facdff783a2693055576f495c7716d096
Christopher Wright <cmwright33@gmail.com>
2013-06-18 16:48:22 -0400

    awesome new update bro added area of square, triangle, circle

--- Calculator.rb
+++ Calculator.rb
@@ -25,52 +25,92 @@ def division(x,y)
   return x / y
 end
 
-def area_of_square
-  return
+def area_of_square(x,y)
+  return x * y
 end
 
 def area_of_circle
-
+  pi = 3.14
+  return pi*(r**2)
 end
 
-def area_of_triangle
+def area_of_triangle(x,y)
+  return 0.5*(x*y)
+end
 
+def get_two_numbers
+    puts "Give me the first number"
+    first = gets.chomp.to_f
+    puts "Give me the second number"
+    second = gets.chomp.to_f
+    return first, second
+end
+
+def get_length_width
+    puts "Give me the length"
+    length = gets.chomp.to_f
+    puts "Give me the width"
+    width = gets.chomp.to_f
+    return length, width
+end
 
 case function
 when "add"
-  puts "what would you like to add"
-  addnum = gets.chomp.to_i
-  puts "how about another number"
-  addnum_2 = gets.chomp.to_i
-  puts add(addnum, addnum_2)
+  x,y = get_two_numbers
+  puts add(x,y)
+
+ # puts "what would you like to add"
+ # addnum = gets.chomp.to_i
+ # puts "how about another number"
+ # addnum_2 = gets.chomp.to_i
+ # puts add(addnum, addnum_2)
 
 when "subtract"
-  puts "what would you like to subtract"
-  subnum = gets.chomp.to_i
-  puts "how about another number"
-  subnum_2 = gets.chomp.to_i
-  puts subtract(subnum,subnum_2)
+  x,y = get_two_numbers
+  puts subtract(x,y)
+
+ # puts "what would you like to subtract"
+ # subnum = gets.chomp.to_i
+ # puts "how about another number"
+ # subnum_2 = gets.chomp.to_i
+ # puts subtract(subnum,subnum_2)
 
 
 when "multiply"
-  puts "what would you like to multiply"
-  multnum = gets.chomp.to_i
-  puts "how about another number"
-  multnum_2 = gets.chomp.to_i
-  puts multiply(multnum, multnum_2)
+  x,y = get_two_numbers
+  puts multiply(x,y)
+
+ # puts "what would you like to multiply"
+ # multnum = gets.chomp.to_i
+ # puts "how about another number"
+ # multnum_2 = gets.chomp.to_i
+ # puts multiply(multnum, multnum_2)
+
+when "divide"
+  x,y = get_two_numbers
+  puts division(x,y)
+
 
 when "area"
   puts "area of what?"
-  area = gets.chomp
+  area = gets.chomp.downcase
     case area
-    when square
-      puts "What is it's length"
+    when "square"
+      x,y = get_length_width
+      puts area_of_square(x,y)
 
-    when circle
-
-    when triangle
-
+#      puts "What is the length?"
+#      length = gets.chomp.to_i
+#      puts "What is the width?"
+#      width = gets.chomp.to_i
+    when "circle"
+      puts "What is the radius?"
+      radius = gets.chomp.to_i
+      puts area_of_circle(radius)
+    when "triangle"
+      x,y = get_length_width
+      puts area_of_triangle(x,y)
     end
 
 end
-end
+
